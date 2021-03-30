36858e073d7baa56d1a8ef5948f1fcc1e5e75488
aakashdeveloper <ahanda205@gmail.com>
2021-03-30 07:51:02 +0530

    map

---
+++ 17functions2.js
@@ -0,0 +1,83 @@
+/*function sayhi(){
+    console.log("Hiii")
+}
+
+sayhi()
+*/
+//IFFI/////
+
+(function(){
+    console.log("Hiii")
+}())
+
+function loop(userinput){
+    for(i=0;i<userinput;i++){
+        console.log(i)
+    }
+}
+
+loop(5)
+0
+1
+2
+3
+4
+
+
+function * loop(userinput){
+    for(i=0;i<userinput;i++){
+       yield i
+    }
+}
+
+var data = loop(4)
+undefined
+data.next()
+{value: 0, done: false}
+data.next()
+{value: 1, done: false}
+data.next()
+{value: 2, done: false}
+data.next()
+{value: 3, done: false}
+data.next()
+{value: undefined, done: true}
+
+//rest
+function add(...args)
+{
+    var sum = 0
+    for(data of args){
+        sum  += data
+    }
+   return sum
+}
+
+add(1,2,3,4,5)
+15
+add(1,2)
+3
+
+
+function test(a,b,c) {
+    const add = a+b+c
+    return add
+}
+
+let arr = [1,2,3]
+
+let arr = [1,2,3]
+undefined
+test(arr)
+"1,2,3undefinedundefined"
+test(...arr)
+6
+
+
+var a = ['a','b','c','d']
+var b = [1,2,...a,3,4]
+b
+(8) [1, 2, "a", "b", "c", "d", 3, 4]
+
+var c = [...a,...b]
+["a", "b", "c", "d", 1, 2, "a", "b", "c", "d", 3, 4]
---
+++ 18mapfilter.js
@@ -0,0 +1,33 @@
+var a = [4,5,3,8,9,34,56,78]
+a.map((data) => {console.log(data)})
+
+a.map((data) => { return data*2 })
+[8, 10, 6, 16, 18, 68, 112, 156]
+
+> map is use to iterate over the Array
+> it always return same length of output array as input array
+> it is use to apply logics (sum,mul,div) return html, bind data
+
+a.map((data) => { return `<p>data*2</p>` })
+(8) ["<p>data*2</p>", "<p>data*2</p>", "<p>data*2</p>", "<p>data*2</p>"]
+
+var b = [20,35,34,26,67,21,19,32]
+b.filter((data) => { return data>25})
+[35, 34, 26, 67, 32]
+
+> filter is use to filter out of the value
+> it may or may not return same length of output
+> it only return those value for which condition/output is true
+
+var b = [20,35,34,26,67,21,19,32]
+b.map((data) => { return data>25})
+[false, true, true, true, true, false, false, true]
+
+
+var a = [0,1,2,3]
+a.map((data) => { return data*2})
+[0,2,4,6]
+
+var a = [0,1,2,3]
+a.filter((data) => { return data*4})
+[1,2,3]
